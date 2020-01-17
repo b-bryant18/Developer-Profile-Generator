114f92556fb94e2d876dc321dc52b66f197dc0f6
Brad Bryant <bradleybryant86@gmail.com>
2020-01-17 00:08:39 -0600

    Put completed html file into htmlGenerator.js and made variables for colors/styling of PDF files

--- htmlGenerator.js
+++ htmlGenerator.js
@@ -40,48 +40,97 @@ const colors = {
 function generateHTML(userInput, response, responseStars, profileImg, githubUsername, userCity, userGithubProfileURL, userblogURL, userBio, numberOfRepos, numberOfFollowers, numberOfUsersFollowing, userCompany) {
     return `
     <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>${userInput.username}</title>
-</head>
-
-<body class = "wrapper">
-<div class="col">
-<header>
-<div class ="photo-header">
-<div class="row"><img src=${profileImg}" alt="profile-img">
-</div>
-<body>
-    <div class="container">
-            <img src="" alt="Profile Image">
-            <h2>Hi, my name is ${username}</h2>
-            <h3>I live in ${userCompany}</h3>
-            <div class = "links-nav">
-            <span class="nav-link"><a href="https://google.com/maps/place/${userCity}"<i class ="fas fa-location-arrow"></i>${userCity}</a></span>
-            <span class="nav link"<a href="${userGithubProfileURL}"><i class="fab fa-github"></i>Github</a></span>
-            <span class="nav link"<a href="${userBlogURL}"><i class="fad fa-rss"></i>Blog</a></span>
-            </div>
-            </div>
-            
-    user github profile -->
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>${userInput.username}</title>
+    </head>
+    
+    <body class="wrapper">
+        <div class="col">
+            <header>
+                <div class="photo-header">
+                    <div class="row"><img src=${profileImg}" alt="profile-img">
+                    </div>
+                    <div class="container">
+                        <h2>Hi, my name is ${response.data.name}</h2>
+                        <h3>I live in ${userCompany}</h3>
+                        <div class="links-nav">
+                            <span class="nav-link"><a href="https://google.com/maps/place/${userCity}" <i
+                                    class="fas fa-location-arrow"></i>${userCity}</a></span>
+                            <span class="nav link"></span><a href="${userGithubProfileURL}"><i
+                                    class="fab fa-github"></i>Github</a></span>
+                            <span class="nav link"></span><a href="${userBlogURL}"><i class="fad fa-rss"></i>Blog</a></span>
+                        </div>
+                    </div>
+                </div>
+            </header>
+    
+            <main class="container">
+                <div class="col">
+                    <h3>${userBio}</h3>
+                </div>
+    
+                <div class="row">
+    
+                    <div class="col">
+                        <div class="card">
+                            <h3>Public Repositories</h3>
+                            <h4>${numberOfRepos}</h4>
+                        </div>
+                    </div>
+    
+                    <div class="col">
+                        <div class="card">
+                            <h3>Starred Projects</h3>
+                            <h4>${responseStars}</h4>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <h3>Followers</h3>
+                            <h4>${numberOfFollowers}</h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <h3>Following</h3>
+                            <h4>${numberOfUsersFollowing}</h4>
+                        </div>
+                    </div>
+    
+                </div>
+    
         </div>
         <div class="bottom-half">
             <!-- User Blog -->
             <!-- User Bio -->
             <div class="row">
-                <div class=col-md-6> Number of public repos </div>
-                <div class=col-md-6> Number of followers </div>
+                <div class="col-md-6"> Number of public repos </div>
+                <div class="col-md-6"> Number of followers </div>
                 <div class="row">
-                    <div class=col-md-6> Number of github stars </div>
-                    <div class=col-md-6> Number of users following </div>
+                    <div class="col-md-6"> Number of github stars </div>
+                    <div class="col-md-6"> Number of users following </div>
                 </div>
             </div>
         </div>
-</body>
-
-</html>`
+    
+        // <!-- closes row -->
+        </div>
+        // <!-- closes container -->
+        </div>
+    
+        </main>
+        </div>
+    </body>
+    
+    </html>`
 }
+
+module.exports = generateHTML;
